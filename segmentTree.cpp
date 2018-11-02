900c6b72b9326ce50587e9636ceda6c549806726
TschieM <37754233+TschieM@users.noreply.github.com>
2018-11-02 22:19:54 +0100

    segment tree updated

--- segmentTree.cpp
+++ segmentTree.cpp
@@ -66,12 +66,12 @@ void update(int i, int value, int j, int l, int r) {
 }
 
 void update_range(int left, int right, int value, int i, int l, int r) {
-  if (left>r || right <l || left>right) {
+  if (left>r || right<l) {
     return ;
   }
-  if (l == r) {
+  if (left<=l && right>=r) {
     tree[i] = value;
-    arr[l] = value;
+    lazy[i] = 1;
     return ;
   }
   int mid = (l+r) / 2;
@@ -81,12 +81,12 @@ void update_range(int left, int right, int value, int i, int l, int r) {
 }
 
 void update_rangex2(int left, int right, int i, int l, int r) {
-  if (left>r || right <l || left>right) {
+  if (left>r || right<l) {
     return ;
   }
-  if (l == r) {
+  if (left<=l && right>=r) {
     tree[i] *= 2;
-    arr[l] *= 2;
+    lazy[i] = 1;
     return ;
   }
   int mid = (l+r) / 2;
@@ -96,7 +96,15 @@ void update_rangex2(int left, int right, int i, int l, int r) {
 }
 
 int query(int i, int l, int r, int left, int right) {
-  if (left>r || right<l || left>right) {
+  if (lazy[i] == 1) {
+    lazy[2*i] = 1;
+    // tree[2*i] = tree[i];
+    tree[2*i] *= 2;
+    lazy[2*i+1] = 1;
+    // tree[2*i+1] = tree[i];
+    tree[2*i+1] *= 2;
+  }
+  if (left>r || right<l) {
     return INF;
   }
   if (left<=l && right>=r) {
